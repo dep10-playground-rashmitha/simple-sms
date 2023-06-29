1257aa44ba15d5efaf0dce9656ad49f85c94b0c2
Anupama Karunarathne <anukar220@gmail.com>
2023-06-29 22:56:23 +0530

    Implemented server logic

--- src/api/StudentHttpController.ts
+++ src/api/StudentHttpController.ts
@@ -32,3 +32,4 @@ router.delete('/:courseId', async (req, res) => {
     const result = await datasource.query('DELETE FROM course WHERE id=?', [req.params.courseId]);
     res.sendStatus(result.affectedRows ? 204 : 404);
 });
+//
