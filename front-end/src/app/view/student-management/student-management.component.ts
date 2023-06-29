7bc45553a2bb36f6c06a6e07a419fa1ac1ddcf49
Rashmitha Piyushan <rashmithapiyushansl@gmail.com>
2023-06-29 20:38:00 +0530

    add the environment

--- front-end/src/app/view/student-management/student-management.component.ts
+++ front-end/src/app/view/student-management/student-management.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class StudentManagementComponent {
 
+  studentAdd(txtName: HTMLInputElement, txtAddress: HTMLInputElement, $event: MouseEvent) {
+    $event.preventDefault();
+    const name=txtName.value.trim();
+    const address=txtAddress.value.trim();
+    const regxName=/^[A-Za-z ]+$/
+    const regxAddress=/^([A-Za-z0-9 ]){3,}/
+    if(!name ||!regxName.test(name)){
+      txtName.select();
+      alert("Name can't be empty or Can't give numbers")
+      return
+    }
+    if(!address ||!regxAddress.test(address)){
+      txtAddress.select();
+      alert("Please enter maximum 3 characters for the address")
+      return
+    }
+
+  }
 }
